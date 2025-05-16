4daa8337593b1b40792820e6b8fe3a1774b0fe2c
OlegBelokonev <lelik.belokonevvv@gmail.com>
2025-05-16 16:09:42 +0200

    rotate array

---
+++ rotate-array/index.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotateArray = (nums, k) => {
+  if (k <= 0) {
+    return;
+  }
+
+  const tail = nums.splice(-k);
+  nums.splice(0, 0, ...tail);
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotateArrayBestPractice = (nums, k) => {
+  {
+    const n = nums.length;
+    k = k % n; // in case k > n
+
+    const reverse = (start, end) => {
+      while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+      }
+    };
+
+    reverse(0, n - 1);       // Reverse entire array
+    reverse(0, k - 1);       // Reverse first k elements
+    reverse(k, n - 1);       // Reverse remaining elements
+  }
+}
+
+rotateArray([1, 2], 3);
+
